337913b2de9df75f8d32cb367280f628e5e3f15b
Mohammed aljasser <justmo59@gmail.com>
2024-08-20 19:41:51 +0300

    add more details

--- block.ts
+++ block.ts
@@ -1,2 +1,7 @@
 // task 1: create a class called Block
+// with the following properties:
+// height: number
+// previousHash: string
+// data: string
+// timestamp: number
 // task 2: create a class called Blockchain
