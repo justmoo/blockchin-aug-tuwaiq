a87409cdf18231fde47727ed71beac86be9963e0
Mohammed aljasser <justmo59@gmail.com>
2024-08-20 21:35:44 +0300

    feat: update blockchain class

--- blockchain.ts
+++ blockchain.ts
@@ -1,1 +1,27 @@
-console.log("Blockchain");
+import Block from "./block";
+class Blockchain {
+  list: Block[] = [];
+
+  constructor() {
+    this.generateGenesisBlock();
+  }
+
+  generateGenesisBlock() {
+    this.list.push(new Block(0, "N/A", "genesis block", Date.now()));
+  }
+
+  addBlock(data: string) {
+    this.list.push(
+      new Block(
+        this.list.length,
+        this.list[this.list.length - 1].hash,
+        data,
+        Date.now()
+      )
+    );
+  }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock("2nd block");
+console.log(blockchain.list);
