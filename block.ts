3f50ffe67179dacaedb9d0361082ac27d1fd7f3d
Mohammed aljasser <justmo59@gmail.com>
2024-08-21 20:26:25 +0300

    feat: update mine function

--- block.ts
+++ block.ts
@@ -7,7 +7,7 @@ export default class Block {
   timestamp: number;
   // task 3 :add hash property
   hash: string = "";
-
+  nonce: number = 0;
   constructor(
     height: number,
     previousHash: string,
@@ -18,13 +18,32 @@ export default class Block {
     this.previousHash = previousHash;
     this.data = data;
     this.timestamp = timestamp;
-    this.hashBlock();
+    this.mine();
   }
 
-  // task 4: add hash function
-  hashBlock() {
-    this.hash = SHA256(
-      this.height + this.previousHash + this.data + this.timestamp
-    ).toString();
+  // task 4: add mine function
+  mine() {
+    // hash the first time
+    let hash = SHA256(
+      this.height + this.previousHash + this.data + this.timestamp + this.nonce
+    );
+    // if the hash is not 000, then mine again
+    while (hash.toString().substring(0, 3) !== "000") {
+      // increment the nonce
+      this.nonce++;
+      // hash again
+      hash = SHA256(
+        this.height +
+          this.previousHash +
+          this.data +
+          this.timestamp +
+          this.nonce
+      );
+      console.log(this.nonce);
+      console.log(hash.toString());
+    }
+    // set the hash
+    this.hash = hash.toString();
+    return this.hash;
   }
 }
